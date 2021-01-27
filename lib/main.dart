58b6e6b7fe1d2570fbc6557077d08275d3f02c78
hyejin-1010 <hyepago@gmail.com>
2021-01-27 15:44:52 +0900

    feat: startup_namer using theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: RandomWords(),
+    );
   }
 }
 
