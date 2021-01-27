56b348a311517c5eb137323f2c257807675b3844
hyejin-1010 <hyepago@gmail.com>
2021-01-27 15:31:18 +0900

    feat: startup_namer

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
+// 'StatelessWidget'을 상속받아 앱 자체를 위젯으로 만든다.
+//  - Fluuter에서는 정렬, 여백, 레이아웃 등 거의 모든 것이 위젯이다.
+// Meterial Library의 Scaffold위젯은 홈 스크린의 위젯 트리를 구성하는 app bar, title, body 속성을 기본으로 제공한다.
+// 위젯의 주된 임무는 다른 하위 위젯을 어떻게 표현할 지를 설명하는 'byild()' 메서드를 제공하는 것이다.
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to Flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
-          ),
-          body: Center(
-            child: Text('Hello World'),
-          ),
-        ));
+    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggetions(),
+    );
+  }
+
+  Widget _buildSuggetions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
