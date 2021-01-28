6c95e0b603a575f0ebacc42e5cbc5fbc7446ee29
hyejin-1010 <hyepago@gmail.com>
2021-01-28 18:26:34 +0900

    feat: page view

---
+++ page_view_study/android/app/src/main/kotlin/com/example/page_view_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.page_view_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ page_view_study/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
+
