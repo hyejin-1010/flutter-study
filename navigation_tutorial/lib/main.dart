54e83bff188981b34dc8149dfeefbfb2d2ed9960
hyejin-1010 <hyepago@gmail.com>
2021-01-28 13:57:59 +0900

    feat: widget animation

--- navigation_tutorial/lib/main.dart
+++ navigation_tutorial/lib/main.dart
@@ -3,75 +3,47 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Passing Data',
-    home: HomeScreen(),
+    home: MainScreen(),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Returning Data Demo'),
+        title: Text('Main Screen'),
       ),
-      body: Center(child: SelectionButton()),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (_) {
+              return DetailScreen();
+            }
+          ));
+        },
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network('https://picsum.photos/250?image=9'),
+        )
+      ),
     );
   }
 }
 
-class SelectionButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: Text('Pick an option, any option!'),
-    );
-  }
-
-  _navigateAndDisplaySelection(BuildContext context) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SelectionScreen())
-    );
-    
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$result')));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pick an option'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context, 'Nope!');
-                },
-                child: Text('Nope.'),
-              ),
-            )
-          ],
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network('https://picsum.photos/250?image=9'),
+          )
         ),
       ),
     );
