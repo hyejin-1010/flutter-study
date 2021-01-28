1f6ecd943a669287067db24b303d98c6fdf78ff2
hyejin-1010 <hyepago@gmail.com>
2021-01-28 12:08:13 +0900

    feat: parent widget manage the widget state

--- managing_state/lib/main.dart
+++ managing_state/lib/main.dart
@@ -4,39 +4,59 @@ void main() {
   runApp(MyApp());
 }
 
-// The widget manages its own state
-class TabboxA extends StatefulWidget {
-  TabboxA({Key key}) : super(key: key);
-
-  _TabboxAState createState() => _TabboxAState();
+// ParentWidget manages the state for TabboxB
+class ParentWidget extends StatefulWidget {
+  _ParentWidgetState createState() => _ParentWidgetState();
 }
 
-class _TabboxAState extends State<TabboxA> {
+class _ParentWidgetState extends State<ParentWidget> {
   bool _active = false;
 
-  void _handleTap() {
+  void _handleTabBoxChanged(bool newValue) {
     setState(() {
-      _active = !_active;
+      _active = newValue;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      child: TabboxB(
+        active: _active,
+        onChanged: _handleTabBoxChanged,
+      ),
+    );
+  }
+}
+
+class TabboxB extends StatelessWidget {
+  TabboxB({Key key, this.active: false, @required this.onChanged})
+    : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTab() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: _handleTap,
+      onTap: _handleTab,
       child: Container(
         child: Center(
           child: Text(
-            _active ? 'Active' : 'Inactive',
+            active ? 'Active' : 'InActive',
             style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.green[600]
+          color: active ? Colors.lightGreen[700] : Colors.green[600]
         ),
-      )
+      ),
     );
   }
 }
@@ -52,7 +72,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          child: TabboxA(),
+          child: ParentWidget(),
         ),
       )
     );
