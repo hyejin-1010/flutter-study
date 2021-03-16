c1b899a2bed093ade204a4d7c6f7bfe9467719ac
hyejin-1010 <hyepago@gmail.com>
2021-03-16 18:56:27 +0900

    Add a drawer to a screen

---
+++ cookbook/drawer_to_a_screen/android/app/src/main/kotlin/com/hellofactory/drawer_to_a_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hellofactory.drawer_to_a_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cookbook/drawer_to_a_screen/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          child: Text('Body'),
+        ),
+      ),
+    );
+  }
+}
+
