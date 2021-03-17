fb016a179186e6cadf5fb3f9408e13fe6b6ccd49
hyejin-1010 <hyepago@gmail.com>
2021-03-17 11:28:37 +0900

    Use themes to share colors and font styles

--- cookbook/use_a_custom_font/lib/main.dart
+++ cookbook/use_a_custom_font/lib/main.dart
@@ -11,17 +11,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Raleway',
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+        fontFamily: 'Georgia',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        )
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Custom Fonts'),
         ),
         body: Center(
-          child: Text(
-            'Robooto Mono sample',
-            style: TextStyle(fontFamily: 'RobotoMono'),
+          child: Container(
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Text Width a background color',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+        ),
+        floatingActionButton: Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow)
+          ),
+          child: FloatingActionButton(
+            onPressed: null,
+            child: Icon(Icons.add),
           ),
         ),
       ),
