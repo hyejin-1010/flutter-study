3fadf8ddd3ccc4ea3c1aa81c7c43c48f98b38c82
hyejin-1010 <hyepago@gmail.com>
2021-03-17 11:23:30 +0900

    Use a custom font

---
+++ cookbook/use_a_custom_font/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Raleway',
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Fonts'),
+        ),
+        body: Center(
+          child: Text(
+            'Robooto Mono sample',
+            style: TextStyle(fontFamily: 'RobotoMono'),
+          ),
+        ),
+      ),
+    );
+  }
+}
