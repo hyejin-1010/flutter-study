14e89c47002a58321234f78eea777b22c0994ab9
hyejin-1010 <hyepago@gmail.com>
2021-03-23 15:34:38 +0900

    StatefulWidget lifecycle

--- study/widget_study/lib/main.dart
+++ study/widget_study/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:widget_study/rabbit.dart';
 import 'package:widget_study/stateful_sample_widget.dart';
@@ -7,8 +9,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _value = 0;
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      setState(() {
+        _value++;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,10 +35,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: StatefulSampleWidget(
+      home: _value < 2 ? StatefulSampleWidget(
         title: "구멍이 있는 박스로 실험하는 자",
+        value: _value,
         rabbit: Rabbit(name: "토끼", state: RabbitState.SLEEP),
-      )
+      ) : Container()
       /*
       home: StatelessSampleWidget(
         title: "구멍이 없는 박스로 실험하는 자",
--- study/widget_study/lib/stateful_sample_widget.dart
+++ study/widget_study/lib/stateful_sample_widget.dart
@@ -6,10 +6,12 @@ import 'package:widget_study/rabbit.dart';
 class StatefulSampleWidget extends StatefulWidget {
   final String title;
   final Rabbit rabbit;
+  final int value;
 
   StatefulSampleWidget({
     this.title,
-    this.rabbit
+    this.rabbit,
+    this.value,
   });
 
   @override
@@ -17,12 +19,13 @@ class StatefulSampleWidget extends StatefulWidget {
 }
 
 class _StatefulSampleWidgetState extends State<StatefulSampleWidget> {
+  // 최초 한 번만 실행된다.
+  // 이 단계에서는 아직 context 가 없기 때문에, context 에 접근할 수 없다.
   @override
   void initState() {
     super.initState();
 
     Timer.periodic(Duration(seconds: 5), (timer) {
-      print(timer.tick);
       int index = timer.tick % 4;
       setState(() {
         switch (index) {
@@ -43,8 +46,41 @@ class _StatefulSampleWidgetState extends State<StatefulSampleWidget> {
     });
   }
 
+  // 최초 한 번만 실행된다. - context 에 접근 가능
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print(MediaQuery.of(context).size);
+  }
+
+  @override
+  void setState(fn) {
+    super.setState(fn);
+  }
+
+  @override
+  void didUpdateWidget(covariant StatefulSampleWidget oldWidget) {
+    // 부모의 상태가 변경됐을 때 호출된다.
+    // oldWidget: 이전 상태의 나 자신
+    super.didUpdateWidget(oldWidget);
+    print("oldWidget : ${oldWidget.value}");
+    print("Widget : ${widget.value}");
+    if (oldWidget.value != widget.value) {
+      print("did update widgt");
+    }
+  }
+
+  @override
+  void dispose() {
+    print("dispose");
+    // TODO: Controller 들 dispose - 안해주면 메모리 누수가 발생할 수 있다.
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // UI를 구성 - State 가 변할 때마다(상태변화가 있을 때마다) 호출
+    // 가장 빈번하게 호출되는 부분
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
